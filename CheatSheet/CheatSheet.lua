a96451c3f46ed539eba65a89c8365c2032f537ff
Sam Kingdon <smarky55@gmail.com>
2015-08-16 22:15:28 +0100

    Core loading of relevant sheets
    Relevant sheets are loaded through index based on current location.
    Prints contents of each sheet's NOTE list to chat.
    Does not yet handle checking any conditions other than location.
    Removed some debug text in OnReload()

--- CheatSheet/CheatSheet.lua
+++ CheatSheet/CheatSheet.lua
@@ -22,6 +22,7 @@ CheatSheet = {}
 local ZoneEvents = {ZONE_CHANGED = true, ZONE_CHANGED_INDOORS = true, ZONE_CHANGED_NEW_AREA = true}
 local EventFrame = CreateFrame("Frame")
 local Index = {}
+local Sheets = {}
 
 --Object Definition--
 CheatSheet.Modules = {}
@@ -35,9 +36,11 @@ function buildIndex()
 	print("Building Index")
 	for modKey, module in pairs(CheatSheet.Modules) do
 		for sheetKey, sheet in pairs(module.SHEETS) do
-			sublist = {}
+			local sublist = {}
 			sublist[1] = modKey
 			sublist[2] = sheetKey
+			local zone
+			local subzone
 			if sheet.ZONE and sheet.SUBZONE then
 				zone = sheet.ZONE
 				subzone = sheet.SUBZONE
@@ -60,27 +63,56 @@ function buildIndex()
 	print("Index Built")
 end
 
+function loadSheets(zone, subzone)
+	-- Reset contenst of Sheets
+	Sheets = {}
+	-- Create sub-index for locationally relevent sheets
+	local tempIndex = {}
+	if Index["ALL"] and Index["ALL"]["ALL"] then
+		for key, sheet in pairs(Index["ALL"]["ALL"]) do
+			table.insert(tempIndex, sheet)
+		end
+	end
+	if Index[zone] and Index[zone]["ALL"] then
+		for key, sheet in pairs(Index[zone]["ALL"]) do
+			table.insert(tempIndex, sheet)
+		end
+	end
+	if subzone and Index[zone] and Index[zone][subzone] then
+		for key, sheet in pairs(Index[zone][subzone]) do
+			table.insert(tempIndex, sheet)
+		end
+	end
+	
+	-- Load content of the relevent sheets
+	for key, sheet in pairs(tempIndex) do
+		table.insert(Sheets, CheatSheet.Modules[sheet[1]].SHEETS[sheet[2]])
+	end
+	
+	-- Debug: Print loaded sheets
+	for key, sheet in pairs(Sheets) do
+		for nkey, note in pairs(sheet.NOTE) do
+			print(note)
+		end
+	end
+end
+
 --Event Handler Functions--
 function OnZoneChange(self, event, ...)
 	print("You are in: " .. GetMinimapZoneText())
+	loadSheets(GetZoneText(), GetSubZoneText())
 end
 
 function OnReload(self, event, ...)
 	print("Game Reloaded")
-	for key, value in pairs(CheatSheet.Modules) do
-		print("Module loaded: " .. value.NAME)
-		for k , v in pairs(value.SHEETS) do
-			print("  Has sheet for: " .. v.SUBZONE .. ":" .. v.ZONE)
-		end
-	end
+	-- for key, value in pairs(CheatSheet.Modules) do
+		-- print("Module loaded: " .. value.NAME)
+		-- for k , v in pairs(value.SHEETS) do
+			-- print("  Has sheet for: " .. v.SUBZONE .. ":" .. v.ZONE)
+		-- end
+	-- end
 	buildIndex()
-	for zone, subzones in pairs(Index) do
-		for subzone, sheets in pairs(subzones) do
-			for key, sheet in pairs(sheets)do
-				print(zone, subzone, sheet[1], sheet[2])
-			end
-		end
-	end
+	loadSheets(GetZoneText(), GetSubZoneText())
 end
 
 
