fbc44c78d88e70c2c10b5988639e1546bb570c8c
Sam Kingdon <smarky55@gmail.com>
2015-08-16 18:03:06 +0100

    Basic sheet indexing
    Implemented sheet indexing based off of respective zones and subzones.
    Working for at least one module.
    Added a few more test sheets to default module.
    Added license boilerplate to lua files.

--- CheatSheet/CheatSheet.lua
+++ CheatSheet/CheatSheet.lua
@@ -1,4 +1,18 @@
---CheatSheet copyright smarky55 (Sam Kingdon) 2015 All rights reserved--
+-- CheatSheet: An addon for World of Warcraft that provides context sensitive info or guidlines
+-- Copyright (C) 2015 Sam "smarky55" Kingdon
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 --Global Variables--
 CheatSheet = {}
@@ -16,6 +30,35 @@ function CheatSheet:Register(module)
 	table.insert(CheatSheet.Modules, module)
 end
 
+--General Functions--
+function buildIndex()
+	print("Building Index")
+	for modKey, module in pairs(CheatSheet.Modules) do
+		for sheetKey, sheet in pairs(module.SHEETS) do
+			sublist = {}
+			sublist[1] = modKey
+			sublist[2] = sheetKey
+			if sheet.ZONE and sheet.SUBZONE then
+				zone = sheet.ZONE
+				subzone = sheet.SUBZONE
+			elseif sheet.ZONE and sheet.SUBZONE == nil then
+				zone = sheet.ZONE
+				subzone = "ALL"
+			elseif sheet.ZONE == nil then
+				zone = "ALL"
+				subzone = "ALL"
+			end	
+			if Index[zone] and Index[zone][subzone] == nil then
+				Index[zone][subzone] = {}
+			elseif Index[zone] == nil then
+				Index[zone] = {}
+				Index[zone][subzone] = {}
+			end
+			table.insert(Index[zone][subzone], sublist)
+		end
+	end
+	print("Index Built")
+end
 
 --Event Handler Functions--
 function OnZoneChange(self, event, ...)
@@ -28,6 +71,14 @@ function OnReload(self, event, ...)
 		print("Module loaded: " .. value.NAME)
 		for k , v in pairs(value.SHEETS) do
 			print("  Has sheet for: " .. v.SUBZONE .. ":" .. v.ZONE)
+		end
+	end
+	buildIndex()
+	for zone, subzones in pairs(Index) do
+		for subzone, sheets in pairs(subzones) do
+			for key, sheet in pairs(sheets)do
+				print(zone, subzone, sheet[1], sheet[2])
+			end
 		end
 	end
 end
---
+++ toWoW.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+XCOPY .\CheatSheet "F:\Programs\World of Warcraft\Interface\AddOns\CheatSheet" /s /i /y
+XCOPY .\CheatSheet_Defaults "F:\Programs\World of Warcraft\Interface\AddOns\CheatSheet_Defaults" /s /i /y
